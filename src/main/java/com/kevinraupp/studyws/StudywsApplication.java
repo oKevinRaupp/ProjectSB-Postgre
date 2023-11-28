9cb02540807edc8679cc2ee19ce69e444e45f55b
oKevinRaupp <rauppke@gmail.com>
2023-11-28 20:26:57 -0300

    first commit

---
+++ src/main/java/com/kevinraupp/studyws/StudywsApplication.java
@@ -0,0 +1,13 @@
+package com.kevinraupp.studyws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudywsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudywsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevinraupp/studyws/StudywsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinraupp.studyws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudywsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
