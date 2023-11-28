06af85100239fd1c8ac59f3c833d23a44b9230ac
oKevinRaupp <rauppke@gmail.com>
2023-11-28 20:47:44 -0300

    h2 database

--- src/main/java/com/kevinraupp/studyws/entities/User.java
+++ src/main/java/com/kevinraupp/studyws/entities/User.java
@@ -1,9 +1,14 @@
 package com.kevinraupp.studyws.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
-
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
