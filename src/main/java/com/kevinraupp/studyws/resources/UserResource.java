89bb7a21432007ca21ab37fa35177eabebdbc069
oKevinRaupp <rauppke@gmail.com>
2023-11-28 21:15:12 -0300

    service layer

--- src/main/java/com/kevinraupp/studyws/resources/UserResource.java
+++ src/main/java/com/kevinraupp/studyws/resources/UserResource.java
@@ -1,17 +1,30 @@
 package com.kevinraupp.studyws.resources;
 
 import com.kevinraupp.studyws.entities.User;
+import com.kevinraupp.studyws.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService userService;
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User("Teste","email@teste.com","321321321","password");
-        return ResponseEntity.ok(user);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.finall();
+        return ResponseEntity.ok(list);
     }
+    @GetMapping(value = "/{id}")
+    public Optional<User> findById(@PathVariable Long id){
+        return userService.findById(id);
+    }
+
 }
---
+++ src/main/java/com/kevinraupp/studyws/services/UserService.java
@@ -0,0 +1,25 @@
+package com.kevinraupp.studyws.services;
+
+import com.kevinraupp.studyws.entities.User;
+import com.kevinraupp.studyws.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> finall(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id){
+        return userRepository.findById(id);
+    }
+
+
+}
