6f82592b2b9817cc185c7a2d9252115f4397c52f
oKevinRaupp <rauppke@gmail.com>
2023-11-29 19:45:57 -0300

    ManyToMany, Category and Product

--- src/main/java/com/kevinraupp/studyws/config/TestConfig.java
+++ src/main/java/com/kevinraupp/studyws/config/TestConfig.java
@@ -34,13 +34,21 @@ public class TestConfig implements CommandLineRunner {
         Category category01 = new Category("Eletronics");
         Category category02 = new Category("Books");
         Category category03 = new Category("Computers");
-        categoryRepository.saveAll(Arrays.asList(category01,category02,category03));
 
         Product p1 = new Product( "Game of Thrones", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
         Product p2 = new Product("Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
         Product p3 = new Product("Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
         Product p4 = new Product("PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
         Product p5 = new Product("Harry Potter", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+        categoryRepository.saveAll(Arrays.asList(category01,category02,category03));
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+
+        p1.getCategories().add(category02);
+        p2.getCategories().add(category01);
+        p2.getCategories().add(category03);
+        p3.getCategories().add(category03);
+        p4.getCategories().add(category03);
+        p5.getCategories().add(category02);
         productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 
         User user01 = new User("Teste01","email@emailteste.com","21312312","password01");
--- src/main/java/com/kevinraupp/studyws/entities/Product.java
+++ src/main/java/com/kevinraupp/studyws/entities/Product.java
@@ -16,7 +16,8 @@ public class Product implements Serializable {
     private String description;
     private Double price;
     private String imgURL;
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "products_category", joinColumns = @JoinColumn(name = "product_id"),inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
     public Product() {
