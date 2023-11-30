89cc4c8873ed182c386c066f2bcf49883787ed27
oKevinRaupp <rauppke@gmail.com>
2023-11-30 18:46:48 -0300

    Exception update

--- src/main/java/com/kevinraupp/studyws/services/UserService.java
+++ src/main/java/com/kevinraupp/studyws/services/UserService.java
@@ -4,6 +4,7 @@ import com.kevinraupp.studyws.entities.User;
 import com.kevinraupp.studyws.repositories.UserRepository;
 import com.kevinraupp.studyws.resources.exceptions.DataBaseException;
 import com.kevinraupp.studyws.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -40,9 +41,13 @@ public class UserService {
 
 
     public User update(Long id,User user){
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity,user);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity,user);
+            return userRepository.save(entity);
+        }catch (EntityNotFoundException e ){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
