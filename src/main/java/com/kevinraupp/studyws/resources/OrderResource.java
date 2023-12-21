315a0f7d8d9e93b26c6426a32ca0e1bd8528c31b
oKevinRaupp <rauppke@gmail.com>
2023-12-21 14:16:09 -0300

    OrderResource ok!

--- src/main/java/com/kevinraupp/studyws/resources/OrderResource.java
+++ src/main/java/com/kevinraupp/studyws/resources/OrderResource.java
@@ -2,6 +2,13 @@ package com.kevinraupp.studyws.resources;
 
 import com.kevinraupp.studyws.entities.Order;
 import com.kevinraupp.studyws.services.OrderService;
+import com.kevinraupp.studyws.util.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,17 +21,31 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/v1/orders")
+@Tag(name = "Order", description = "Endpoint for Managing orders.")
 public class OrderResource {
     @Autowired
     private OrderService orderService;
 
-    @GetMapping
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Finds all orders", description = "Finds all orders", tags = {"Order"},
+            responses = {@ApiResponse(description = "Success", responseCode = "200", content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Order.class)))}),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
     public ResponseEntity<List<Order>> findAll() {
         List<Order> list = orderService.finall();
         return ResponseEntity.ok(list);
     }
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    @Operation(summary = "Finds a order", description = "Finds a order by passing an {id}", tags = {"Order"},
+            responses = {@ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = Order.class))),
+                    @ApiResponse(description = "No content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)})
     public Optional<Order> findById(@PathVariable Long id) {
         return orderService.findById(id);
     }
